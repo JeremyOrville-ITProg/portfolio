1a505c01c47dd476cf068d4291a290675feaa2d3
JeremyOrville-ITProg <jorvilledesiderio@gmail.com>
2026-05-10 18:04:20 +0800

    Added a new project card
    Changed project card preview logic so I can reuse it with other cards

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Optimization for smaller screens //
 const burger = document.getElementById('burger');
 const navigation = document.getElementById('navigation');
 
@@ -6,7 +7,7 @@ if (burger && navigation) {
         navigation.classList.toggle('active');
     });
 };
-
+// Background Music //
 function playAudio(){
     var bgm = document.getElementById("bgm");
 
@@ -18,12 +19,30 @@ function playAudio(){
     }
 }
 
+// Bark //
 function bark(){
     var bark = document.getElementById("bark");
         bark.play();
 }
 
+// Background hover effect to highlight mouse pointer //
 document.addEventListener("mousemove", (e) => {
     document.documentElement.style.setProperty("--x", e.clientX + "px");
     document.documentElement.style.setProperty("--y", e.clientY + "px");
+});
+
+const previews = document.querySelectorAll(".project-preview");
+
+previews.forEach((img) =>{
+
+    const activeSrc = img.dataset.active;
+    const staticSrc = img.dataset.static;
+    
+    img.addEventListener("mouseenter", () => {
+        img.src = activeSrc;
+    });
+
+    img.addEventListener("mouseleave", () => {
+        img.src = staticSrc;
+    });
 });
