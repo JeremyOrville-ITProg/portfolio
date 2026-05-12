8feaa49558f506444a638bd0970a7ded476fee4f
JeremyOrville-ITProg <jorvilledesiderio@gmail.com>
2026-05-13 04:06:51 +0800

    changed project card reactiveness
    has a gloss effect now when hovered, acts like a 3d card reacting to mouse position (like in the Pokemon TCG Online game)

--- script.js
+++ script.js
@@ -51,6 +51,41 @@ cards.forEach((card) =>{
     });
 });
 
+cards.forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = (centerY - y) / 15;
+        const rotateY = (x - centerX) / 15;
+
+        card.style.transform = `
+            perspective(1000px)
+            rotateX(${rotateX}deg)
+            rotateY(${rotateY}deg)
+            scale(1.02)
+        `;
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform = `
+            perspective(1000px)
+            rotateX(0deg)
+            rotateY(0deg)
+            scale(1)
+        `;
+    });
+
+});
+
 //Plays swipeup/down depending on if current section is lower/higher than target section type shi//
 
 const swipeup = document.getElementById("swipeup");
